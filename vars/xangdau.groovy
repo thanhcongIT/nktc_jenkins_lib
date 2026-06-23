748f693ef6857707e3d269ece3464b7a8442b76f
nguyenkieuthanh cong <khiconkklot@outlook.com.vn>
2026-06-23 22:57:24 +0700

    ci

--- vars/xangdau.groovy
+++ vars/xangdau.groovy
@@ -64,7 +64,7 @@ def buildApp() {
                             sh '''
                                 rm -rf node_modules
                                 rm -f package-lock.json
-                                npm i --force
+                                npm i 
                                 npx ng build
                             '''
                         }
